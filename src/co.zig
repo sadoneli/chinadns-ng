f2f75c7fdd5d8ff6db0d9ef17a96881223ece2ed
sadog <sadoneli@gmail.com>
2026-01-22 16:22:01 +0800

    co: fix coroutine frame allocation tracking

--- src/co.zig
+++ src/co.zig
@@ -1,12 +1,43 @@
 const std = @import("std");
-const cc = @import("cc.zig");
 const g = @import("g.zig");
 const assert = std.debug.assert;
 
+const FrameAlloc = struct {
+    frame_ptr: [*]u8,
+    base: [*]u8,
+    len: usize,
+    alignment: u29,
+    terminated: bool = false,
+    next: ?*FrameAlloc = null,
+};
+
 /// create and start a new coroutine
 pub fn start(comptime func: anytype, args: anytype) void {
-    const buf = g.allocator.alignedAlloc(u8, std.Target.stack_align, @frameSize(func)) catch unreachable;
-    _ = @asyncCall(buf, {}, func, args);
+    const Frame = @Frame(func);
+    const frame_align = @alignOf(Frame);
+    const frame_size = @frameSize(func);
+    const alloc_len = frame_size + (frame_align - 1);
+
+    const base = g.allocator.alignedAlloc(u8, frame_align, alloc_len) catch unreachable;
+    const base_addr = @ptrToInt(base.ptr);
+    const frame_addr = std.mem.alignForward(base_addr, frame_align);
+    const frame_ptr = @intToPtr([*]u8, frame_addr);
+
+    const fa = g.allocator.create(FrameAlloc) catch unreachable;
+    fa.* = .{
+        .frame_ptr = frame_ptr,
+        .base = base.ptr,
+        .len = alloc_len,
+        .alignment = @intCast(u29, frame_align),
+        .terminated = false,
+        .next = _frames,
+    };
+    _frames = fa;
+
+    const FrameBuf = []align(frame_align) u8;
+    const frame_buf = @ptrCast(FrameBuf, frame_ptr[0..frame_size]);
+    _ = @asyncCall(frame_buf, {}, func, args);
+
     check_terminated();
 }
 
@@ -18,9 +49,15 @@ pub fn do_resume(frame: anyframe) void {
 
 /// called when the coroutine is about to terminate: `defer co.terminate(@frame(), frame_size)`
 pub fn terminate(top_frame: anyframe, frame_size: usize) void {
-    assert(_terminated == null);
-    _terminated = top_frame;
-    _frame_size = frame_size;
+    _ = frame_size;
+    const frame_ptr = @intToPtr([*]u8, @ptrToInt(top_frame));
+    var it = _frames;
+    while (it) |fa| : (it = fa.next) {
+        if (fa.frame_ptr == frame_ptr) {
+            fa.terminated = true;
+            break;
+        }
+    }
 }
 
 // ========================================================================
@@ -38,15 +75,25 @@ pub fn data(comptime T: type) *T {
 
 // ========================================================================
 
-var _terminated: ?anyframe = null;
-var _frame_size: usize = 0;
+var _frames: ?*FrameAlloc = null;
 
 /// free the memory of a terminated coroutine
 fn check_terminated() void {
-    const top_frame = _terminated orelse return;
-    _terminated = null;
+    var prev: ?*FrameAlloc = null;
+    var cur = _frames;
+    while (cur) |fa| {
+        const next = fa.next;
+        if (fa.terminated) {
+            if (prev) |p|
+                p.next = next
+            else
+                _frames = next;
 
-    // https://github.com/ziglang/zig/issues/10622
-    const ptr = @intToPtr([*]u8, @ptrToInt(top_frame));
-    return g.allocator.free(ptr[0.._frame_size]);
+            g.allocator.rawFree(fa.base[0..fa.len], fa.alignment, @returnAddress());
+            g.allocator.destroy(fa);
+        } else {
+            prev = fa;
+        }
+        cur = next;
+    }
 }
